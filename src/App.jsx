16cb1224f0231a897534e424085ceed587065965
Miguel Diaz <playnoblock99@gmail.com>
2026-07-25 22:47:18 -0400

    feat: avance de HomePage.jxs y App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MascotasList from "./components/mascotas/MascotasList"; 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-<></>
-  )
+    <>
+      <BrowserRouter>
+        <header>
+          <nav>
+            <NavLink to="/">Inicio</NavLink>
+            <NavLink to="/mascotas">Mascotas</NavLink>
+            <NavLink to="/mascotas/nueva">Publicar Mascota</NavLink>
+          </nav>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/mascotas" element={<MascotasList />} />
+
+            {/* Agregar esto cuando este listo:
+            <Route path="/mascotas/nueva" element={<MascotasForm />} />
+            */}
+            
+            <Route path="*" element={<h2>404 - Página no encontrada</h2>} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;

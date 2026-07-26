c6a24a341c6d748d73c29c41dd8a236a92a46c19
peschke_araya@outlook.com <peschke_araya@outlook.com>
2026-07-26 19:44:48 -0400

    feat: agrega redireccion al home tras crear mascota en CrearMascotaPage

--- src/pages/mascotas/CrearMascotaPage.jsx
+++ src/pages/mascotas/CrearMascotaPage.jsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom"
 import MascotasForm from "../../components/mascotas/MascotasForm"
 
 function CrearMascotaPage() {
+    const navigate = useNavigate()
+
+    const ManejoMascotaCreada = () => {
+        navigate('/')
+    }
+
     return (
         <div>
             <h1>Registrar Mascota</h1>
-            <MascotasForm />
+            <MascotasForm onMascotaCreada={ManejoMascotaCreada} />
         </div>
     )
 }

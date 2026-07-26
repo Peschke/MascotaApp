27b7767bc4917d2b79c2658cbe0e4fa651b54a6e
Miguel Diaz <playnoblock99@gmail.com>
2026-07-26 02:50:47 -0400

    fix: correcciones varias

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,21 @@ import MascotasList from "./components/mascotas/MascotasList";
 function App() {
   return (
     <>
-        <header>
-          <nav>
-            |
-            <NavLink to="/">Inicio</NavLink>|
-            |
-            <NavLink to="/mascotas">Mascotas</NavLink>|
-            |
-            <NavLink to="/mascotas/nueva">Publicar Mascota</NavLink>|
-          </nav>
-        </header>
+      <header>
+        <nav>
+          <NavLink to="/" end>Inicio</NavLink> | 
+          <NavLink to="/mascotas">Mascotas</NavLink> | 
+          <NavLink to="/mascotas/nueva">Publicar Mascota</NavLink>
+        </nav>
+      </header>
 
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/mascotas" element={<MascotasList />} />
-
-            {/* Agregar esto cuando este listo:
-            <Route path="/mascotas/nueva" element={<MascotasForm />} />
-            */}
-            
-            <Route path="*" element={<h2>404 - Página no encontrada</h2>} />
-          </Routes>
-        </main>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/mascotas" element={<MascotasList />} />
+          <Route path="*" element={<h2>404 - Página no encontrada</h2>} />
+        </Routes>
+      </main>
     </>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    BaseURL: "https://mascotas.pythonanywhere.com/api/",
+    baseURL: "https://mascotas.pythonanywhere.com/api/",
 });
 
 export default api;

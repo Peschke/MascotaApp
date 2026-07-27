ad4015e9576c324c93bd6668eca11ffb1b9e5544
peschke_araya@outlook.com <peschke_araya@outlook.com>
2026-07-27 02:32:38 -0400

    style: agrega clases y css para MascotaDetailPage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,46 +1,44 @@
 import { NavLink } from "react-router-dom";
+import "./HomePage.css";
 
 function HomePage() {
-    return (
-        <div>
-            <section>
-                <h1>Bienvenido a la Red de Mascotas</h1>
-                <p>
-                    Encuentra publicaciones sobre mascotas en adopción, perdidas o historias de la comunidad.
-                </p>
-
-                <div>
-                    |
-                    <NavLink to="/mascotas">
-                        Ver todas las mascotas
-                    </NavLink>
-                    |
-                    <NavLink to="/mascotas/nueva">
-                        Publicar una mascota
-                    </NavLink>
-                    |
-                </div>
-            </section>
-
-            {/* Sección de características o accesos rápidos */}
-            <section>
-                <div>
-                    <h3>Registro</h3>
-                    <p>Explora publicaciones detalladas con fotos e información sobre cada mascota.</p>
-                </div>
-
-                <div>
-                    <h3>Comunidad</h3>
-                    <p>Comenta en las publicaciones de otros usuarios.</p>
-                </div>
-
-                <div>
-                    <h3>Publicación Fáci</h3>
-                    <p>Agrega a tu mascotabdesde nuestra API.</p>
-                </div>
-            </section>
+  return (
+    <div className="inicio">
+      <section className="principal">
+        <h1>Bienvenido a la Red de Mascotas</h1>
+        <p>
+          Encuentra publicaciones sobre mascotas en adopción, perdidas o historias de la comunidad. 
+        </p>
+        
+        <div className="acciones">
+          <NavLink to="/mascotas" className="boton">
+            Ver todas las mascotas
+          </NavLink>
+          
+          <NavLink to="/mascotas/nueva" className="boton">
+            Publicar una mascota
+          </NavLink>
         </div>
-    );
+      </section>
+
+      <section className="bloques">
+        <div className="tarjeta">
+          <h3>Registro</h3>
+          <p>Explora publicaciones detalladas con fotos e información sobre cada mascota.</p>
+        </div>
+
+        <div className="tarjeta">
+          <h3>Comunidad</h3>
+          <p>Comenta en las publicaciones de otros usuarios.</p>
+        </div>
+
+        <div className="tarjeta">
+          <h3>Publicación Fácil</h3>
+          <p>Agrega a tu mascota desde nuestra API.</p>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default HomePage;

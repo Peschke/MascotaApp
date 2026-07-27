a8359a5939b73cddd5b48320f251f38f1b1966b3
peschke_araya@outlook.com <peschke_araya@outlook.com>
2026-07-27 03:10:56 -0400

    style: agrega clases y css para CrearMascotaPage

--- src/pages/mascotas/CrearMascotaPage.jsx
+++ src/pages/mascotas/CrearMascotaPage.jsx
@@ -1,19 +1,22 @@
-import { useNavigate } from "react-router-dom"
-import MascotasForm from "../../components/mascotas/MascotasForm"
+import { useNavigate } from "react-router-dom";
+import MascotasForm from "../../components/mascotas/MascotasForm";
+import "./CrearMascotaPage.css";
 
 function CrearMascotaPage() {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const ManejoMascotaCreada = () => {
-        navigate('/')
-    }
+        navigate("/");
+    };
 
     return (
-        <div>
-            <h1>Registrar Mascota</h1>
-            <MascotasForm onMascotaCreada={ManejoMascotaCreada} />
+        <div className="pagina">
+            <h1 className="titulo"></h1>
+            <div className="bloque">
+                <MascotasForm onMascotaCreada={ManejoMascotaCreada} />
+            </div>
         </div>
-    )
+    );
 }
 
-export default CrearMascotaPage
+export default CrearMascotaPage;

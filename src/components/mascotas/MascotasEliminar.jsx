b542e37c68518cae4e0d56f0ac26161185b61c9b
Dfvg898 <diegohd641@gmail.com>
2026-07-26 23:30:23 -0400

    agregando manejo de error  y listando la mascota con el id ingresado

--- src/components/mascotas/MascotasEliminar.jsx
+++ src/components/mascotas/MascotasEliminar.jsx
@@ -4,9 +4,11 @@ import mascotasApi from "../../services/api";
 
 function MascotasEliminar() {
 
+    //buscar mascota por id
     const [mascota, setMascota] = useState([]);
-    
     const [idBusqueda, setIdBusqueda] = useState("");
+    //error
+    const [error, setError] = useState("");
 
     const fetchEliminarMascota = async (id) => {
         try {
@@ -16,13 +18,29 @@ function MascotasEliminar() {
 
         }catch (error) {
             console.error(error);
+            manejarError(error);
         }
     }
+
+    const manejarError = (error) => {
+        console.error(error);
+
+        const status = error.response?.status;
+
+        if (status === 404) {
+            setError("No se encontró la mascota.");
+        } else if (status === 400) {
+            setError("Los datos ingresados no son válidos. Revisa los campos.");
+        } else {
+            setError("Ocurrió un error. Intenta nuevamente más tarde.");
+        }
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (!idBusqueda) return;
         fetchEliminarMascota(idBusqueda);
+        setError(""); // Limpiar el mensaje de error al realizar la busqueda
     }
 
   return (
@@ -40,10 +58,24 @@ function MascotasEliminar() {
                 />
                 </label>
                 <button >Buscar Mascota</button>
+                <p>{error}</p>
                 
                 
 
             </form>
+
+            <div>
+                <h3>{mascota.nombre}</h3>
+
+                <img src={mascota.imagen} alt={mascota.nombre} />
+                <p><strong>Descripción:</strong> {mascota.descripcion}</p>
+                <p><strong>Estado:</strong> {mascota.estado}</p>
+                <p><strong>Tipo Animal:</strong> {mascota.tipo_animal}</p>
+                <p><strong>Edad:</strong> {mascota.edad}</p>
+                <p><strong>Raza:</strong> {mascota.raza}</p>
+                <p><strong>Sexo:</strong> {mascota.sexo}</p>
+                <p><strong>Tamaño:</strong> {mascota.tamano}</p>
+            </div>
     </>
   );
 }
